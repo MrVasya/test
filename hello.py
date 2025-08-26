6534b0965e59303c1c51680d4c03d82e2eb95e9b
MrVasya <vasya.tylypnikov@gmail.com>
2025-08-26 10:18:06 +0300

    remove flaglink

--- hello.py
+++ hello.py
@@ -7,8 +7,8 @@ USER = "MrVasya"
 NAME = "test"
 FILENAME = "flaglink"
 
-DELAY_START = 0.7     # 0 секунд
-DELAY_END = 1     # 0.9 секунд
+DELAY_START = 0.75     # 0 секунд
+DELAY_END = 0.9     # 0.9 секунд
 STEP = 0.01         # 10 мс
 
 FLAG = "FLAG"       # что ищем в теле
